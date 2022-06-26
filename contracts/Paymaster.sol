ac1b9df8d07101ac90f7654fd31152bba5397b6e
AlexeySadkovich <sadkovich01@mail.ru>
2022-06-26 22:14:03 +0300

    add checks on paying and all pricings view

--- contracts/Paymaster.sol
+++ contracts/Paymaster.sol
@@ -6,15 +6,17 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * @author Alexey Sadkovich
- * @notice A contract for creating payments plans.
+ * @notice A contract for creating subscription or one-time payments plans.
  */
 contract Paymaster is Ownable {
     using Counters for Counters.Counter;
 
     struct Pricing {
+        uint256 id;
         string name;
         uint256 price;
         uint256 period;
+        string metadata;
     }
 
     struct Payment {
@@ -33,7 +35,15 @@ contract Paymaster is Ownable {
         _pricingsCounter.increment(); // set counter to 1
     }
 
+    /// @notice Creates payment for sender with chosen pricing.
+    /// @param pricingId Id of the pricing to pay.
     function pay(uint256 pricingId) external payable {
+        require(pricingId < _pricingsCounter.current(), "Unkwown pricing id");
+        require(
+            msg.value > pricings[pricingId].price,
+            "Value must be equal to price."
+        );
+
         payments[msg.sender].pricingId = pricingId;
         payments[msg.sender].timestamp = block.timestamp;
 
@@ -43,38 +53,65 @@ contract Paymaster is Ownable {
         }
     }
 
+    /// @notice Returns pricing id and payment status if it exists.
+    /// @param payer The address of user to get payment information about.
+    /// @return pricingId Id of the pricing paid by user.
+    /// @return paid Status of the payment. Returns true if user has paid one
+    /// of pricings and false otherwise.
     function getPayment(address payer)
         external
         view
         onlyOwner
-        returns (uint256 tokenId, bool payed)
+        returns (uint256 pricingId, bool paid)
     {
         if (!payments[payer].exists) {
             return (0, false);
         }
 
-        uint256 pricingId = payments[payer].pricingId;
+        pricingId = payments[payer].pricingId;
 
         if (
+            pricings[pricingId].period == 0 ||
             payments[payer].timestamp + pricings[pricingId].period <
             block.timestamp
         ) {
-            return (pricingId, false);
+            return (pricingId, true);
         }
 
-        return (pricingId, true);
+        return (pricingId, false);
     }
 
+    /// @notice Adds new pricing to the contract.
+    /// @param name Some name for the pricing.
+    /// @param price Price that user has to pay.
+    /// @param period Period of time (in seconds) for the payments. Set to 0
+    /// if pricinig doesn't implies subscription and it requires to be paid only once.
+    /// @param metadata A string with some additional data. Can be empty if doesn't required.
     function addPricing(
         string memory name,
         uint256 price,
-        uint256 period
+        uint256 period,
+        string memory metadata
     ) external onlyOwner returns (bool success) {
         uint256 pricingId = _pricingsCounter.current();
         _payersCounter.increment();
 
-        pricings[pricingId] = Pricing(name, price, period);
+        pricings[pricingId] = Pricing(pricingId, name, price, period, metadata);
 
         return true;
+    }
+
+    /// @notice Returns all pricings stored in the contract.
+    function getAllPricings()
+        public
+        view
+        returns (Pricing[] memory allPricings)
+    {
+        Pricing[] memory res = new Pricing[](_pricingsCounter.current() - 1);
+        for (uint256 i = 0; i < _pricingsCounter.current() - 1; i++) {
+            res[i] = pricings[i];
+        }
+
+        return res;
     }
 }
